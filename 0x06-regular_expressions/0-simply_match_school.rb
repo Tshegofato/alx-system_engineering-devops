225850bb60b2c5d3fee9c043112e9a82d436a6a9
Tshegofato <basetsana_12@icloud.com>
2023-11-29 11:53:39 -0800

    Simply matching School

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,16 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_school(argument)
-  regex = /School/
-  if argument.match?(regex)
-    puts "The argument '#{argument}' contains the word 'School'."
-  else
-    puts "The argument '#{argument}' does not contain the word 'School'."
-  end
-end
-
-if ARGV.length != 1
-  puts "Usage: ruby script_name.rb <argument>"
-else
-  match_school(ARGV[0])
-end
+puts ARGV[0].scan(/School/).join
