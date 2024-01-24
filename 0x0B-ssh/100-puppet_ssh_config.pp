ae79afc71063a7ab826d55c687117e55bf88280b
Tshegofato <basetsana_12@icloud.com>
2024-01-24 09:49:10 -0800

    Client configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env puppet
 # Puppet manifest to configure SSH client settings.
 
 # Define SSH client configuration
@@ -10,7 +10,15 @@ file { '/home/your_username/.ssh/config':
   group   => 'your_username',
 }
 
+# Set correct permissions for the .ssh directory
+file { '/home/your_username/.ssh':
+  ensure => directory,
+  mode   => '0700',
+  owner  => 'your_username',
+  group  => 'your_username',
+}
+
 # Notify user about the changes
 notify { 'SSH configuration updated successfully':
-  require => File['/home/Tshegofato/.ssh/config'],
+  require => File['/home/your_username/.ssh/config'],
 }
