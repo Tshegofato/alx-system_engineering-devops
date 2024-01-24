2d69fa04415644f9aab7507179b6cbd88ead70c7
Tshegofato <basetsana_12@icloud.com>
2024-01-24 13:03:25 -0800

    add

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,26 +1,8 @@
-#!/usr/bin/pup
+ #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-
-package {
-   'python3.8.':
-  ensure => '3.3.10',
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
 
-package {
-   'python3-pip':
-  ensure => installed,
-}
 
-package {
-   'flask':
-  ensure => '2.1.0',
-  require => package['python3-pip'],
-  provider => 'pip',
-}
-
-package {
-  'werkzeug':
-  ensure => '2.2.2',
-  require => package['python3-pip'],
-  provide => 'pip',
-}
