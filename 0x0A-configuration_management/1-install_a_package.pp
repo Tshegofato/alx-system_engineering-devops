0df17f8440afc51bce48d2f61045467fc1242d73
Tshegofato <basetsana_12@icloud.com>
2024-01-30 18:53:33 -0800

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,23 +1,6 @@
-#!/usr/bin/pup
-
-# Install python3-pip
-exec { 'install_python3_pip':
-  command => '/usr/bin/apt-get install -y python3-pip',
-  path    => ['/usr/bin'],
-}
-
-# Install an specific version of Flask (2.1.0) using pip3
-package { 'flask':
+##!/usr/bin/pup
+# Using Puppt to install flask from pip3
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Exec['install_python3_pip'],
+  provider => 'pip3'
 }
-
-# Install werkzeug
-exec { 'install_werkzeug':
-  command => '/usr/bin/pip3 install werkzeug',
-  path    => ['/usr/bin'],
-  require => Exec['install_python3_pip'],
-}
-
-
